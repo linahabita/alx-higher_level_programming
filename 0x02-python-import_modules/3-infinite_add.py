739a30eaff19bf8b650c3cb7a412dee13377cd9a
lina <habita.lina@univ-ouargla.dz>
2023-11-05 13:51:56 -0800

    python3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     sum = 0
-    for 1 in range(len(sys.argv)-1):
+    for i in range(len(sys.argv)-1):
         sum += int (sys.argv(i + 1))
     print (sum)
     
