ab7f3ba6682a4e0a8d624cbb763779f69ef59def
linahabita <habita.lina@univ-ouargla.dz>
2024-06-19 00:04:33 +0000

    J

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-/* a script that prints "javascript is amazing"*/
-const myVar = "JavaScript is amazing";
+/* a script that prints "javascript is amazing" */
+const myVar = 'JavaScript is amazing';
 console.log(myVar);
